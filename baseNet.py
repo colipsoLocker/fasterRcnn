895c830c0502f7822254293bbc63166dda13e837
colipso <colipso@126.com>
2018-10-15 23:54:36 +0800

    Ready to fix RoiPoolingConv layer

---
+++ baseNet.py
@@ -0,0 +1,1 @@
+from tensorflow.keras.layers import Input , Add , Dense , Activation , Flatten , Convolution2D , MaxPool2D , ZeroPadding2D , AveragePooling2D , TimeDistributed
---
+++ train.py
@@ -0,0 +1,4 @@
+from tensorflow.keras.optimizers import Adam , SGD , RMSprop
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import Progbar
