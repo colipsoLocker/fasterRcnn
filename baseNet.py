Fix wrong shape BUG

baseNet.py
@@ -107,9 +107,9 @@ def rpn(base_layers, num_anchors):
     x_class = Conv2D(num_anchors, (1, 1), activation='sigmoid', kernel_initializer='uniform', name='rpn_out_class')(x)
     #x_regr 输出的是对应的四个坐标
     x_regr = Conv2D(num_anchors * 4, (1, 1), activation='linear', kernel_initializer='zero', name='rpn_out_regress')(x)
-    print("Debug rpn layer output shape")
-    print(np.shape(x_class))
-    print(np.shape(x_regr))
+    #print("Debug rpn layer output shape")
+    #print(np.shape(x_class))
+    #print(np.shape(x_regr))
     return [x_class, x_regr, base_layers]
 
 

losses.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 from tensorflow.keras.losses import   binary_crossentropy , categorical_crossentropy
-from tensorflow.keras.backend import abs , sum , cast , less_equal , mean
+from tensorflow.keras.backend import abs , sum , cast , less_equal , mean 
+from tensorflow.keras import backend as K
+import numpy as np
+
+
 
 lambda_rpn_regr = 1.0
 lambda_rpn_class = 1.0
@@ -11,21 +15,28 @@ lambda_cls_class = 1.0
 epsilon = 1e-4
 
 def rpn_loss_regr(num_anchors):
+    #print('rpn_loss_regr')
     def rpn_loss_regr_fixed_num(y_true, y_pred):
+        #print('rpn_loss_regr')
+        #print(np.shape(y_true))
+        #print(np.shape(y_pred))
         x = y_true[:, :, :, 4 * num_anchors:] - y_pred   #rpn网络输出的是(0,width,heigh , 4*anchors)
-        x_abs = abs(x)
-        x_bool = cast(less_equal(x_abs, 1.0), tf.float32)
-        print('rpn_loss_regr')
-        return lambda_rpn_regr * sum(
-            y_true[:, :, :, :4 * num_anchors] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / sum(epsilon + y_true[:, :, :, :4 * num_anchors])
+        x_abs = K.abs(x)
+        x_bool = K.cast(K.less_equal(x_abs, 1.0), tf.float32)
+        #print('rpn_loss_regr')
+        return lambda_rpn_regr * K.sum(
+            y_true[:, :, :, :4 * num_anchors] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / K.sum(epsilon + y_true[:, :, :, :4 * num_anchors])
 
     return rpn_loss_regr_fixed_num
 
 
 def rpn_loss_cls(num_anchors): #rpn网络的类别损失函数
+    #print('rpn_loss_cls')
     def rpn_loss_cls_fixed_num(y_true, y_pred):
-        print('rpn_loss_cls')
-        return lambda_rpn_class * sum(y_true[:, :, :, :num_anchors] * binary_crossentropy(y_pred[:, :, :, :], y_true[:, :, :, num_anchors:])) / sum(epsilon + y_true[:, :, :, :num_anchors])
+        #print('rpn_loss_cls')
+        #print(np.shape(y_true))
+        #print(np.shape(y_pred))
+        return lambda_rpn_class * K.sum(y_true[:, :, :, :num_anchors] * K.binary_crossentropy(y_pred[:, :, :, :], y_true[:, :, :, num_anchors:])) / K.sum(epsilon + y_true[:, :, :, :num_anchors])
        
     return rpn_loss_cls_fixed_num
 

train.py
@@ -75,7 +75,7 @@ model_all.compile(optimizer='sgd', loss='mae')
 
 
 #手工定义训练阶段
-epoch_length = 1000
+epoch_length = config.epoch_length
 num_epochs = int(config.num_epochs)
 iter_num = 0
 
@@ -107,18 +107,18 @@ for epoch_num in range(num_epochs):
 				if mean_overlapping_bboxes == 0:
 					print('RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
 
-			print('Begin to generate data for train')
+			#print('Begin to generate data for train')
 			X, Y, img_data = next(data_gen_train) #np.copy(x_img), [np.copy(y_rpn_cls), np.copy(y_rpn_regr)], img_data_aug 用来给分类网络的输入
 
-			print('Begin to train model_rpn')
+			#print('Begin to train model_rpn')
 			#test
-			print(np.shape(X))
-			print(np.shape(Y[0]),'   ',np.shape(Y[1]))
-			P_rpn = model_rpn.predict_on_batch(X)
-			print(np.shape(P_rpn[0]),'   ',np.shape(P_rpn[1]))
+			#print(np.shape(X))
+			#print(np.shape(Y[0]),'   ',np.shape(Y[1]))
+			#P_rpn = model_rpn.predict_on_batch(X)
+			#print(np.shape(P_rpn[0]),'   ',np.shape(P_rpn[1]))
 			#endtest
 			loss_rpn = model_rpn.train_on_batch(X, Y) #训练model_rpn
-			print('Predict rpn')
+			#print('Predict rpn')
 			P_rpn = model_rpn.predict_on_batch(X) #测试rpn
 
 			#R:boxes, probs  返回经过npm后剩下的bbox以及对应的probs   （（左上，右下坐标），序号）    （anchors， 序号 ）

utils.py
@@ -420,8 +420,8 @@ def calc_rpn(img_data, width, height, resized_width, resized_height, img_length_
     y_rpn_overlap = np.transpose(y_rpn_overlap, (2, 0, 1)) #把值放在第一个纬度，
     y_rpn_overlap = np.expand_dims(y_rpn_overlap, axis=0) #为后续送入网络方便，增加第0纬
     #debug
-    print("y_rpn_overlap shape:")
-    print(np.shape(y_rpn_overlap))
+    #print("y_rpn_overlap shape:")
+    #print(np.shape(y_rpn_overlap))
     #enddebug
 
 
@@ -449,15 +449,22 @@ def calc_rpn(img_data, width, height, resized_width, resized_height, img_length_
         val_locs = random.sample(range(len(neg_locs[0])), len(neg_locs[0]) - num_pos) #保持正负样本均衡
         y_is_box_valid[0, neg_locs[0][val_locs], neg_locs[1][val_locs], neg_locs[2][val_locs]] = 0
 
+    #debug
+    #print('calc_rpn before')
+    #print(np.shape(y_is_box_valid))
+    #print(np.shape(y_rpn_overlap))
+    #print(np.shape(y_rpn_regr))
+    #enddebug
+
     y_rpn_cls = np.concatenate([y_is_box_valid, y_rpn_overlap], axis=1) #矩阵合并，y_rpn_cls：是否包含类，其前半段是该anchor是否可用
     y_rpn_regr = np.concatenate([np.repeat(y_rpn_overlap, 4, axis=1), y_rpn_regr], axis=1) #y_rpn_regr：回归梯度，前半段包含是否有效
 
     #debug
-    print('calc_rpn')
-    print(np.shape(y_is_box_valid))
-    print(np.shape(y_rpn_overlap))
-    print(np.shape(y_rpn_cls))
-    print(np.shape(y_rpn_regr))
+    #print('calc_rpn after')
+    #print(np.shape(y_is_box_valid))
+    #print(np.shape(y_rpn_overlap))
+    #print(np.shape(y_rpn_cls))
+    #print(np.shape(y_rpn_regr))
     #enddebug
     return np.copy(y_rpn_cls), np.copy(y_rpn_regr) #返回 (num_anchors ,output_height, output_width, ) (num_anchors*4 ,output_height, output_width )
 
@@ -478,13 +485,13 @@ def get_anchor_gt(all_img_data, class_count,  img_length_calc_function,  C = con
 
     while True:
         #debug
-        i = 0
+        #i = 0
         #enddebug
         if mode == 'train':
             np.random.shuffle(all_img_data)
 
         for img_data in all_img_data:
-            print(time.asctime( time.localtime(time.time()) ) + 'Get anchor begin deal img_data')
+            #print(time.asctime( time.localtime(time.time()) ) + 'Get anchor begin deal img_data')
             try:
                 if C.balanced_classes and sample_selector.skip_sample_for_balanced_class(img_data):
                     continue
@@ -509,7 +516,7 @@ def get_anchor_gt(all_img_data, class_count,  img_length_calc_function,  C = con
                 # resize the image so that smalles side is length = 600px 用PIL读图，用cv2来处理是个不错的方法，在test中已验证
                 x_img = cv2.resize(x_img, (resized_width, resized_height), interpolation=cv2.INTER_CUBIC)
                 
-                print('Get resized x_img')
+                #print('Get resized x_img')
 
                 try:
                     #返回 (num_anchors ,output_height, output_width) (num_anchors*4 ,output_height, output_width )
@@ -537,8 +544,8 @@ def get_anchor_gt(all_img_data, class_count,  img_length_calc_function,  C = con
                 y_rpn_cls = np.transpose(y_rpn_cls, (0, 2, 3, 1))  # 0 ， 长， 宽 ， 值
                 y_rpn_regr = np.transpose(y_rpn_regr, (0, 2, 3, 1)) # 0 ， 长， 宽 ， 值
                 #debug
-                print('yield')
-                print(np.shape(x_img) , np.shape(y_rpn_cls) , np.shape(y_rpn_regr))
+                #print('yield')
+                #print(np.shape(x_img) , np.shape(y_rpn_cls) , np.shape(y_rpn_regr))
                 #enddebug
                 yield np.copy(x_img), [np.copy(y_rpn_cls), np.copy(y_rpn_regr)], img_data_aug
 